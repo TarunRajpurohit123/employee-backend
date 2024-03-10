5122322abce1df4231f02fcbed9e517d8e740cb3
Tarun <tarunrajpurohitcreator@gmail.com>
2024-03-10 13:58:51 +0530

    setup backend template

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api",require("./src/routers/index"));
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
